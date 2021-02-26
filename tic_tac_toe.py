2cc17f45528818c5c4bbcb09f744a12e13b34c2f
yuhan1212 <yuhan.chi12@gmail.com>
2021-02-26 15:06:30 -0800

    add game_opening and draw_status

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,53 +1,98 @@
-import pygame as pg,sys
+import pygame as pg, sys
 from pygame.locals import *
 import time
 
-# set up golbal variables and background size
-XO = 'x'
-winner = None
-draw_result = False
-width = 400
-height = 400
-white = (255, 255, 255)
-line_color = (100,10,10)
 
-#TicTacToe 3x3 board
-TTT = [[None]*3,[None]*3,[None]*3]
+'''Change the images here'''
 
-#initializing pygame window
+#loading the images 
+opening = pg.image.load('images/tic-tac-toe.png')
+x_img = pg.image.load('images/X.png')
+o_img = pg.image.load('images/O.png')
+
+
+'''Keep the rest unchanged'''
+
+# take size of Tic Tac Toe (TTT) from user request (size range 9 ~ 100 square number)
+TTT_size = int(input("Enter the Tic Tac Toe square size you want to play: "))
+while TTT_size not in [9, 16, 25, 49, 64, 81, 100]:
+	print("Sorry. The size should be a SQUARE number [9, 16, 25, 49, 64, 81, 100].")
+	TTT_size = int(input("Enter the Tic Tac Toe size you want to play: "))
+
+# the beginning status for TTT
+OX_symbol: str = "O"     # start from 'O', and consists of O and X
+winner = None            # not have winner yet
+draw = False             # if all box filled and no winner, then draw
+
+# set up original data and initialize global variables
+_TTT_SIDE = int(TTT_size ** 0.5)               # if TTT_size is 9, TTT_side is 3
+_SQUARE: int = 600                             # playground window size
+_BOX_Length = _SQUARE / _TTT_SIDE              # length of each TTT box
+_MARGIN = _BOX_Length / 4              # margin for each image and shapes drew
+_IMAGE_SIZE: int = int(_BOX_Length - _MARGIN)  # image will be put in each box
+_STATUS_HEIGHT: int = 100            # the status space is _SQUARE * _STATUS_HEIGHT
+
+# set the name appears at the top of the display window
+title = str(_TTT_SIDE) + "X" + str(_TTT_SIDE) + " Tic Tac Toe" 
+
+# TicTacToe customized board, using list represent TTT, start consisting all None
+TTT = []
+for i in range (_TTT_SIDE):
+    TTT += [[None] * _TTT_SIDE]
+
+# initializing pygame window
 pg.init()
 fps = 30
 CLOCK = pg.time.Clock()
-screen = pg.display.set_mode((width, height + 100), 0, 32)
+screen = pg.display.set_mode((_SQUARE, _SQUARE + _STATUS_HEIGHT),0,32)
+pg.display.set_caption(title)
 
-# set the name appears at the top of the display window
-pg.display.set_caption("Tic Tac Toe")
+# resizing images and background
+x_img = pg.transform.scale(x_img, (_IMAGE_SIZE, _IMAGE_SIZE))
+o_img = pg.transform.scale(o_img, (_IMAGE_SIZE, _IMAGE_SIZE))
+opening = pg.transform.scale(opening, (_SQUARE, _SQUARE + _STATUS_HEIGHT))
 
-#loading the images
-opening = pg.image.load('images/opening.png')
-x_img = pg.image.load('images/x.png')
-o_img = pg.image.load('images/o.png')
 
-#resizing images
-x_img = pg.transform.scale(x_img, (80, 80))
-o_img = pg.transform.scale(o_img, (80, 80))
-opening = pg.transform.scale(opening, (width, height+100))
-
-# function to start and restart the game
 def game_opening():
     # In pygame, the blit() function is used on the surface
     # to draw an image on top of another image.
+    screen.blit(opening,(0,0))
     # pg.display.update() is used to update the change
-    screen.blit(opening,(0, 0))
     pg.display.update()
     # wait for one second
     time.sleep(1)
-    screen.fill(white)
+    screen.fill((255,255,255))
+    
     # Drawing vertical lines
-    pg.draw.line(screen,line_color, (width / 3, 0), (width / 3, height), 7)
-    pg.draw.line(screen,line_color, (width / 3 * 2, 0), (width / 3 * 2, height), 7)
+    i = 0
+    while i < _TTT_SIDE + 1:
+        pg.draw.line(screen,(0,0,0),(_BOX_Length * i, 0),(_BOX_Length * i, _SQUARE),7)
+        i += 1
+    
     # Drawing horizontal lines
-    pg.draw.line(screen,line_color, (0,height / 3), (width, height / 3), 7)
-    pg.draw.line(screen,line_color, (0,height / 3 * 2), (width, height / 3 * 2), 7)
+    i = 0
+    while i < _TTT_SIDE + 1:
+        pg.draw.line(screen,(0,0,0),(0, _BOX_Length * i),(_SQUARE, _BOX_Length * i),7)
+        i += 1
+    
     draw_status()
 
+
+def draw_status():
+    global draw
+
+    if winner is None:
+        message = OX_symbol.upper() + "'s Turn"
+    else:
+        message = winner.upper() + " won!"
+    if draw:
+        message = 'Game Draw!'
+
+    font = pg.font.Font(None, 50)
+    text = font.render(message, 1, (255, 255, 255))
+
+    # copy the rendered message onto the board
+    screen.fill ((0, 0, 0), (0, _SQUARE, _SQUARE + _STATUS_HEIGHT, _STATUS_HEIGHT))
+    text_rect = text.get_rect(center=(_SQUARE / 2, _SQUARE + _STATUS_HEIGHT / 2))
+    screen.blit(text, text_rect)
+    pg.display.update()
